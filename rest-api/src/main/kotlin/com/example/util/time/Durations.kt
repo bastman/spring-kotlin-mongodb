0ada18871535fcd9eebc0efba1b55c062e2c572e
sebastian.schmidt <bastman77@gmail.com>
2019-03-24 09:46:59 +0100

    initial

--- rest-api/src/main/kotlin/com/example/util/time/Durations.kt
+++ rest-api/src/main/kotlin/com/example/util/time/Durations.kt
@@ -4,4 +4,4 @@ import java.time.Duration
 import java.time.Instant
 
 fun Instant.durationToNow(now: Instant = Instant.now()): Duration = Duration.between(this, now)
-fun Instant.durationToNowInMillis(now: Instant = Instant.now()):Long = durationToNow(now).toMillis()
+fun Instant.durationToNowInMillis(now: Instant = Instant.now()): Long = durationToNow(now).toMillis()
